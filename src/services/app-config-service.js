846fc7bdadc84d5b16907d4fcce6764547a245ca
Alberto Chamorro <albertchc04@gmail.com>
2018-07-15 12:08:18 -0600

    add interceptor response and reject error with middleware

--- src/services/app-config-service.js
+++ src/services/app-config-service.js
@@ -5,4 +5,16 @@ const httpManagerService = http.create({
   baseUrl: appConfig.apiUrl
 })
 
+const responseMiddleware = (res) => {
+  console.log('interceptor success -> ', res)
+  return res
+}
+
+const rejectMiddleware = (err) => {
+  console.log('interceptor error -> ', err)
+  return Promise.reject(err)
+}
+
+httpManagerService.after(responseMiddleware, rejectMiddleware)
+
 export default httpManagerService
