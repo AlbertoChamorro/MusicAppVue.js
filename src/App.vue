837c6127fe7b7020b6028387ef3066c924b71c68
Alberto Chamorro <albertchc04@gmail.com>
2018-07-15 22:35:51 -0600

    implement slot in component notification with BULMA

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   #app.has-background-white-ter
 
     app-header
-        
+    
+    app-notification(v-show="showNotification")
+      p(slot="message") No se encontraron resultados.
+
     section.section
       nav.nav.has-shadow
         .container
@@ -49,16 +52,18 @@
   import AppFooter from '@/components/layouts/Footer.vue'
   import AppTrack from '@/components/tracks/Track.vue'
   import AppLoader from '@/components/shared/Loader.vue'
+  import AppNotification from '@/components/shared/Notification.vue'
 
   export default {
     name: 'app',
-    components: { AppHeader, AppFooter, AppTrack, AppLoader },
+    components: { AppHeader, AppFooter, AppTrack, AppLoader, AppNotification },
     data () {
       return {
         searchText: '',
         tracks: [],
         selectedTrackId: null,
-        isLoading: false
+        isLoading: false,
+        showNotification: false
       }
     },
     methods: {
@@ -67,11 +72,13 @@
           this.clearSearch()
           return
         }
+        this.tracks = []
         this.isLoading = true
         trackService.getAll(this.searchText)
           .then(res => {
             // https://api.spotify.com/v1/search?query=the+be&type=track&offset=0&limit=20
             this.isLoading = false
+            this.showNotification = res.tracks.total === 0
             this.tracks = res.tracks.items
           })
       },
@@ -82,6 +89,15 @@
       playMusic: function (id) {
         console.log(`playing music ${id}`)
         this.selectedTrackId = id
+      }
+    },
+    watch: {
+      showNotification () {
+        if (this.showNotification) {
+          setTimeout(() => {
+            this.showNotification = false
+          }, 2500)
+        }
       }
     },
     computed: {
---
+++ src/components/shared/Notification.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    .container
+        .columns
+            .column.is-10.is-offset-1
+                .notification.is-danger
+                    slot(name="message") Algo anduvo mal :-(
+
+</template>
+<style lang="scss" scoped>
+    .notification{
+        margin-top: 10px;
+    }
+</style>
