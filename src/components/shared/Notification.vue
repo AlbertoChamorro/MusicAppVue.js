d4a840c5215e7aa95fca8a045c7c2d7b75ce3e61
Alberto Chamorro <albertchc04@gmail.com>
2018-07-15 22:54:26 -0600

    dynamic slot notification

--- src/components/shared/Notification.vue
+++ src/components/shared/Notification.vue
@@ -2,10 +2,18 @@
     .container
         .columns
             .column.is-10.is-offset-1
-                .notification.is-danger
+                .notification(:class="isError ? 'is-danger' : 'is-primary'")
                     slot(name="message") Algo anduvo mal :-(
 
 </template>
+<script>
+export default {
+  props: {
+    isError: { type: Boolean, default: false }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
     .notification{
         margin-top: 10px;
